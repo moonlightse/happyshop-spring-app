1061eb9e734f47d6f467d68f7458a6855f061640
longphamit <longphamjustdoit@gmail.com>
2021-07-27 22:51:07 +0700

    update

---
+++ happ-shop/src/main/java/com/moonlight/happshop/HappShopApplication.java
@@ -0,0 +1,13 @@
+package com.moonlight.happshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappShopApplication.class, args);
+	}
+
+}
